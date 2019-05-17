329b740f4aa0e9afa48d0653c91af8537eb3ef51
Simon Oulevay <simon.oulevay@gmail.com>
2019-05-17 11:13:57 +0200

    Fix git issues on agent

--- agent/run-jenkins-agent.sh
+++ agent/run-jenkins-agent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-exec docker run -i --rm --name jenkins-agent --init jenkins/slave:3.29-1-alpine java -jar /usr/share/jenkins/slave.jar
+exec docker run -i --rm --name jenkins-agent --init jenkins-experiments/agent java -jar /usr/share/jenkins/slave.jar
